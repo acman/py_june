942efaab511977d9bb0639f6a82ebf540cab09e5
Vitalii Hrebennikov <webasman@gmail.com>
2023-12-23 14:04:13 +0200

    create posts

---
+++ posts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=500, blank=True)
+    author = models.ForeignKey('users.ForumUser', on_delete=models.CASCADE)
+    category = models.ForeignKey('categories.Category', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "posts"
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
--- py_june/settings.py
+++ py_june/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'categories.apps.CategoriesConfig',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
