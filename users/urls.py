fb2910e6bae418598d45df333dbdce3dc77bd4ca
NykoEugen <nyko.eugen@gmail.com>
2023-12-27 19:08:34 +0200

    pj-13-logout. Add logout button.

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from .views import SignUpView, LogInView
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LogInView.as_view(), name='login'),
-
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout')
 ]
