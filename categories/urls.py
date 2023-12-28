cb00a68bf786409e21ed75178f9b7a9062012648
Vitalii Hrebennikov <webasman@gmail.com>
2023-12-28 05:30:05 +0200

    add url namespace

--- categories/urls.py
+++ categories/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from categories import views
 
+app_name = 'categories'
+
 urlpatterns = [
-    path('category_list/', views.category_list, name='category_list'),
+    path('category_list/', views.category_list, name='list'),
 ]
