14448aeb6c5beb6caa0decf8857916edc805ce98
NykoEugen <nyko.eugen@gmail.com>
2023-12-29 20:28:24 +0200

    pj-13-logout, fix, test

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,5 @@ app_name = "users"
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", LogInView.as_view(), name="login"),
-    path('logout/', LogoutView.as_view(next_page='home'), name='logout')
+    path("logout/", LogoutView.as_view(next_page="users:login"), name="logout"),
 ]
