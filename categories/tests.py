40a1c338c654d38789197d3342ec2f3564f1e3fd
NykoEugen <nyko.eugen@gmail.com>
2024-01-05 19:02:07 +0200

    WIP. pj-17-category-page

--- categories/tests.py
+++ categories/tests.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from categories.models import MainCategory
+from categories.models import Category, MainCategory
+from posts.models import Post
 
 
 class CategoryListViewTest(TestCase):
@@ -15,3 +17,35 @@ class CategoryListViewTest(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Category 1")
+
+
+class CategoryDetailViewTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            password="testpassword123",
+        )
+        self.main_category = MainCategory.objects.create(name="MainCategory")
+        self.category = Category.objects.create(
+            name="Test category",
+            description="Test description",
+            main_category=self.main_category,
+        )
+
+        self.post = Post.objects.create(
+            title="Test Post",
+            content="Test content",
+            author=self.user,
+            category=self.category,
+            created_at="2023-01-01T00:00:00Z",
+            updated_at="2023-01-01T00:00:00Z",
+            is_active=True,
+        )
+
+    def test_category_detail(self):
+        response = self.client.get(
+            reverse("categories:detail", kwargs={"category_id": self.category.pk})
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test Post")
--- categories/views.py
+++ categories/views.py
@@ -1,10 +1,22 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from categories.models import MainCategory
+from categories.models import Category, MainCategory
+from posts.models import Post
 
 
 def category_list(request: HttpRequest) -> HttpResponse:
     objects = MainCategory.objects.prefetch_related("category_set").all()
 
     return render(request, "categories/category_list.html", {"objects": objects})
+
+
+def category_detail(request: HttpRequest, category_id: int) -> HttpResponse:
+    objects = Post.objects.filter(category_id=category_id, is_active=1)
+    category = Category.objects.filter(pk=category_id)
+
+    return render(
+        request,
+        "categories/category_detail.html",
+        {"objects": objects, "category": category},
+    )
