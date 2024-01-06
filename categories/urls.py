73cd1f8a680f4e9a6f62d194a6d61b431c395e02
Vitalii Hrebennikov <webasman@gmail.com>
2024-01-06 13:04:58 +0200

    show categories on /forum/ url

--- categories/urls.py
+++ categories/urls.py
@@ -5,6 +5,6 @@ from categories import views
 app_name = "categories"
 
 urlpatterns = [
-    path("category_list/", views.category_list, name="list"),
-    path("category_detail/<int:category_id>/", views.category_detail, name="detail"),
+    path("", views.category_list, name="list"),
+    path("<int:category_id>/", views.category_detail, name="detail"),
 ]
