eb0ab2cfa4772b6f51cfb821d36005b9703f3664
Vitalii Hrebennikov <webasman@gmail.com>
2024-01-06 14:45:46 +0200

    add test data mixin

--- core/tests.py
+++ core/tests.py
@@ -1,8 +1,35 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+
+from categories.models import MainCategory, Category
+from posts.models import Post
 
 
 class HomePageTest(TestCase):
     def test_home_page_get_success(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
+
+
+class TestDataMixin:
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            password="testpassword123",
+        )
+        self.main_category = MainCategory.objects.create(title="MainCategory")
+        self.category = Category.objects.create(
+            title="Test category",
+            slug="test-category",
+            description="Test description",
+            main_category=self.main_category,
+        )
+
+        self.post = Post.objects.create(
+            title="Test Post",
+            slug="test-post",
+            content="Test content",
+            author=self.user,
+            category=self.category,
+        )
