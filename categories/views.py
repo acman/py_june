4b361a0f79ca2dca210cc7f3d0ca67e9c52475bb
NykoEugen <nyko.eugen@gmail.com>
2024-01-10 00:33:35 +0200

    WIP. pj-8-forum-page

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count, Q, Case, When, Value, IntegerField
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
 
@@ -6,9 +7,10 @@ from posts.models import Post
 
 
 def category_list(request: HttpRequest) -> HttpResponse:
-    objects = MainCategory.objects.prefetch_related("category_set").all()
+    objects = MainCategory.objects.prefetch_related("category_set", "category_set__post_set").all()
 
-    return render(request, "categories/category_list.html", {"objects": objects})
+    return render(request, "categories/category_list.html",
+                  {"objects": objects})
 
 
 def category_detail(request: HttpRequest, category_slug: str) -> HttpResponse:
