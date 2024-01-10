728efe4a612815f9cf5135a6080cf810b14769bb
Vitalii Hrebennikov <webasman@gmail.com>
2024-01-11 00:31:59 +0200

    render forum

--- categories/views.py
+++ categories/views.py
@@ -1,18 +1,35 @@
-from django.db.models import Count, Q, Case, When, Value, IntegerField
+from itertools import groupby
+from operator import attrgetter
+
 from django.core.paginator import Paginator
+from django.db import models
+from django.db.models import Count, OuterRef, Subquery, Prefetch
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from categories.models import Category, MainCategory
+from comments.models import Comment
 from posts.models import Post
 
 
 def category_list(request: HttpRequest) -> HttpResponse:
-    objects = MainCategory.objects.prefetch_related(
-        "categories", "categories__posts", "categories__posts__comments").all()
+    # FIXME: Probably not the best way to do this
+    last_comment_subquery = Comment.objects.filter(
+        post__category=OuterRef('pk')
+    ).order_by('-created_at').values('title')[:1]
 
-    return render(request, "categories/category_list.html",
-                  {"objects": objects})
+    categories = Category.objects.annotate(
+        post_count=Count('posts', distinct=True),
+        comment_count=Count('posts__comments', distinct=True),
+        last_comment=Subquery(last_comment_subquery, output_field=models.CharField())
+    ).select_related('main_category')
+
+    main_categories = MainCategory.objects.all()
+    context = {
+        'main_categories': main_categories,
+        'categories': categories,
+    }
+    return render(request, "categories/category_list.html", context)
 
 
 def category_detail(request: HttpRequest, category_slug: str) -> HttpResponse:
