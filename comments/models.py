131e5a9bb90a7c75286604b33d77d3effa5d807a
Vitalii Hrebennikov <webasman@gmail.com>
2024-01-10 21:58:44 +0200

    change related names

--- comments/models.py
+++ comments/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Comment(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=500, blank=True)
-    author = models.ForeignKey("users.ForumUser", on_delete=models.CASCADE)
-    post = models.ForeignKey("posts.Post", on_delete=models.CASCADE)
+    author = models.ForeignKey("users.ForumUser", on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey("posts.Post", on_delete=models.CASCADE, related_name="comments")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
