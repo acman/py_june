d04910e38acdd992f66bc2d62ba0f0cfdeda2a10
NykoEugen <nyko.eugen@gmail.com>
2024-01-11 19:29:07 +0200

    pj-53-56, add posts(last, hot), add posts need help, add activities

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.db.models import Count
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from comments.models import Comment
 from posts.models import Post
 
 
@@ -16,6 +17,7 @@ def home(request: HttpRequest) -> HttpResponse:
     need_help_posts = help_posts.annotate(
         comment_count=Count("comments", distinct=True)
     ).filter(comment_count=0)[:5]
+    user_activities = Comment.objects.all().order_by("-updated_at")[:5]
 
     return render(
         request,
@@ -24,5 +26,6 @@ def home(request: HttpRequest) -> HttpResponse:
             "last_posts": last_posts,
             "most_hot": most_hot,
             "need_help_posts": need_help_posts,
+            "user_activities": user_activities,
         },
     )
