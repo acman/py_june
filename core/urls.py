53ba949d08060d8e473e0a5becc7c6db338e2dbd
NykoEugen <nyko.eugen@gmail.com>
2024-01-11 16:54:11 +0200

    pj-53-54, add posts(last, hot)

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import include, path
 
 from core import views
 
 urlpatterns = [
     path("", views.home, name="home"),
 ]
+
--- core/views.py
+++ core/views.py
@@ -1,6 +1,20 @@
+from random import choice
+
+from django.db.models import Count
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from categories.models import Category
+from posts.models import Post
+
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, "home.html")
+    last_posts = Post.objects.filter(is_active=True).order_by("-updated_at")[:5]
+    most_hot = Post.objects.annotate(
+        comment_count=Count("comments", distinct=True)).order_by("-comment_count").first()
+    last_post = choice(last_posts)
+
+    return render(request, "home.html", {
+        "last_post": last_post,
+        "most_hot": most_hot,
+    })
